6bd8589ab5c08aa1534f9a2d62fe415f7fa7ee25
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-07-13 20:08:52 +0500

    minorbugsfix

--- main.cpp
+++ main.cpp
@@ -2,30 +2,34 @@
 #include <vector>
 
 double currentbalance = 0;
-double diposingmoney = 0;
-double withdrawmoneyy = 0;
+double vardipositmoney = 0;
+double varwithdrawmoney = 0;
 
 void depositmoney()
 {
-    std::cout << "Enter Monay to Deposit: ";
-    std::cin >> diposingmoney;
-    currentbalance += diposingmoney;
+    std::cout << "Enter Money to Deposit: ";
+    std::cin >> vardipositmoney;
+    currentbalance += vardipositmoney;
 }
 void withdrawmoney()
 {
-    std::cout << "Enter Monay to Withdraw: ";
-    std::cin >> withdrawmoneyy;
-    currentbalance -= withdrawmoneyy;
+    if(varwithdrawmoney >= currentbalance){
+        std::cout << "Oops! Something Went Wrong.\n Withdraw amount is large can't perform operation";
+        
+    }
+    std::cout << "Enter Money to Withdraw: ";
+    std::cin >> varwithdrawmoney;
+    currentbalance -= varwithdrawmoney;
 }
 void checkcurrentbalance()
 {
-    std::cout << "Your current balanace in M.M bank is " << currentbalance << " /=\n";
+    std::cout << "Your current balance in M.M bank is " << currentbalance << " /=\n";
 }
 void accountbalance()
 {
     std::cout << "\nUsername: Muhammad Mustafa\n";
     std::cout << "Passkey: 25082010\n";
-    std::cout << "Your current balanace: " << currentbalance << " /=\n\n";
+    std::cout << "Your current balance: " << currentbalance << " /=\n\n";
 }
 int main()
 {
@@ -34,38 +38,45 @@ int main()
     std::cout << "=================================\n\n";
     char userchoice;
     int bankuserchoice;
+    do
+    {
+        { // User Bank Options
+            std::cout << "Please enter Valid no\n";
+            std::cout << "---------------------------\n";
+            std::cout << "1.Deposit Money\n";
+            std::cout << "2.Withdraw Money\n";
+            std::cout << "3.Check Current Balance\n";
+            std::cout << "4.Account Details\n";
+            std::cout << "5.Exit\n";
+            std::cout << "---------------------------\n";
+            std::cout << "\nEnter Number: ";
+            std::cin >> bankuserchoice;
+        }
 
-    { // User Bank Otions
-        std::cout << "Please enter Valid no\n";
-        std::cout << "---------------------------\n";
-        std::cout << "1.Deposit Money\n";
-        std::cout << "2.Withdraw Money\n";
-        std::cout << "3.Check Current Balance\n";
-        std::cout << "4.Account Detils\n";
-        std::cout << "5.Exit\n";
-        std::cout << "---------------------------\n";
-        std::cout << "\nEnter Number: ";
-        std::cin >> bankuserchoice;
-    }
-    if (bankuserchoice == 1)
-    {
-        depositmoney();
-    }
-    else if (bankuserchoice == 2)
-    {
-        withdrawmoney();
-    }
-    else if (bankuserchoice == 3)
-    {
-        checkcurrentbalance();
-    }
-    else if (bankuserchoice == 4)
-    {
-        accountbalance();
-    }
-    else if (bankuserchoice == 5){
-        // return 0;
-    }
+        if (bankuserchoice == 1)
+        {
+            depositmoney();
+        }
+        else if (bankuserchoice == 2)
+        {
+            withdrawmoney();
+        }
+        else if (bankuserchoice == 3)
+        {
+            checkcurrentbalance();
+        }
+        else if (bankuserchoice == 4)
+        {
+            accountbalance();
+        }
+        else if (bankuserchoice == 5)
+        {
+            
+        }
+        std::cout << "\nDo you want to continue the Program: ";
+        std::cin >> userchoice;
+        std::cout << "\n";
+    } while (userchoice == 'y' || userchoice == 'Y');
 
     return 0;
 }
